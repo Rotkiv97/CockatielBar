a8da4633740bdc17fdb6a26e98d6a8c190606309
victor guidoni <guidonivictor3@gmail.com>
2025-03-21 11:52:28 +0100

    updata

--- CocktailDebacle/CocktailDebacle.Server/obj/Debug/net8.0/CocktailDebacle.Server.AssemblyInfo.cs
+++ CocktailDebacle/CocktailDebacle.Server/obj/Debug/net8.0/CocktailDebacle.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CocktailDebacle.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+489976877ec15bbaf704e21bca064119ee3fe409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+923f3ab6158e9e13655649c1c1a2e3f01033a40d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CocktailDebacle.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CocktailDebacle.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
