4e1dcf16dbbd99e75ba689e573e6917b51b8bc6a
MtNite <112702620+mttmrz@users.noreply.github.com>
2025-03-29 20:56:00 +0100

    Delete SfidaTerranova-side directory

--- SfidaTerranova-side/SfidaTerranova/CocktailDebacle/init-db/init.sql
+++
@@ -1,8 +0,0 @@
-IF NOT EXISTS(SELECT * FROM sys.databases WHERE name='CocktailDb')
-BEGIN
-    CREATE DATABASE CocktailDb;
-    PRINT 'Database CocktailDb created successfully';
-END
-GO
-
--- Aggiungi qui eventuali script di inizializzazione tabelle
