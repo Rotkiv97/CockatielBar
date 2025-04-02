5d115055931d23e9f593b440be28060b7946b13c
victor guidoni <guidonivictor3@gmail.com>
2025-04-02 15:53:38 +0100

    upDate

--- SfidaTerranova/CocktailDebacle/CocktailDebacle.Server/obj/Debug/net8.0/CocktailDebacle.Server.AssemblyInfo.cs
+++ SfidaTerranova/CocktailDebacle/CocktailDebacle.Server/obj/Debug/net8.0/CocktailDebacle.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CocktailDebacle.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99d74219e44be60886d19c6929fd62c238c81df1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ccd77981bf792243b7eedd3771c38a1ec29817c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CocktailDebacle.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CocktailDebacle.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
